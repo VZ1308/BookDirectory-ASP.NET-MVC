75e1a2bd3a22271e79fa52dd792e9742977854f6
Viktoria <154434964+VZ1308@users.noreply.github.com>
2024-10-21 16:54:22 +0200

    Update BookRepository.cs

--- BookDirectory/Repositories/BookRepository.cs
+++ BookDirectory/Repositories/BookRepository.cs
@@ -2,5 +2,24 @@
 {
     public class BookRepository
     {
+        private static List<Book> books = new List<Book>();
+        private static int nextId = 1;
+
+        public List <Book> GetAllBooks() => books;
+
+        public void AddBook(Book book) 
+        {
+            book.Id = nextId++;
+            books.Add(book);
+        }
+
+        public void DeleteBook(int id) 
+        {
+            var bookToRemove = books.FirstOrDefault(b => b.Id == id);
+            if (bookToRemove != null)
+            {
+                books.Remove(bookToRemove);
+            }
+        }    
     }
 }
