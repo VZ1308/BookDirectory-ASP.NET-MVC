ae3553bc2bf1c4686893953623d53c22783de2c8
Viktoria <154434964+VZ1308@users.noreply.github.com>
2024-10-21 17:09:24 +0200

    Update BooksController.cs

--- BookDirectory/Controllers/BooksController.cs
+++ BookDirectory/Controllers/BooksController.cs
@@ -1,6 +1,38 @@
 ﻿namespace BookDirectory.Controllers
 {
-    public class BooksController
+    public class BooksController : Controller
     {
+    private readonly BookRepository _repository; // Wert kann nur im Konstruktor gesetzt werden
+    
+    //Konstruktor initialisiert BookRepository
+    public Bookscontroller()
+    {
+        _repository = new BookRepository();
+    }
+
+   
+    public IActionResult Index()
+    {
+        var books = _repository.GetAllBooks();
+        return View(books);
+    }
+    [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.AddBook(book);
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _repository.DeleteBook(id);
+            return RedirectToAction("Index");
+        }
     }
 }
+    
+
